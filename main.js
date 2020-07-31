a96ae6136bce6349d29ddbb9dc862f209de2dd1e
Glitch (hello-webpage) <none>
2020-07-31 00:19:12 +0000

    🐈🌎 Checkpoint ./main.js:17678999/21

--- main.js
+++ main.js
@@ -31,12 +31,12 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    let weight = Math.round(300/14)+" stone";
-    let temperature =  Math.round((94-32)/(9/5))+" centigrade";
+    var weight = Math.round(300/14)+" stone";
+    var temperature =  Math.round((94-32)/(9/5))+" centigrade";
     newStory = newStory.replace("94 fahrenheit", temperature).replace("300 pounds", weight);
 
   }
 
-  story.textContent = ;
+  story.textContent = newStory ;
   story.style.visibility = 'visible';
 }
